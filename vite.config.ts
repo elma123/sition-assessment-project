441639863f8d6e527a3c1858c1799d93e3beae9a
Elma Geurts <elma.geurts@gmail.com>
2025-10-16 19:54:27 +0200

    Not include wayfinder if Vercel, no php available

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { wayfinder } from '@laravel/vite-plugin-wayfinder'
-import tailwindcss from '@tailwindcss/vite'
-import vue from '@vitejs/plugin-vue'
-import laravel from 'laravel-vite-plugin'
-import { defineConfig } from 'vite'
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 const isVercel = process.env.VERCEL === '1' // ✅ Vercel sets this env variable automatically
 
@@ -22,7 +22,9 @@ export default defineConfig({
                 },
             },
         }),
-        // 👇 Only include wayfinder if not on Vercel
-        !isVercel && wayfinder(),
+        // Only include wayfinder if not on Vercel
+        !isVercel && wayfinder({
+            formVariants: true,
+        }),
     ].filter(Boolean),
-})
+});
